5ed7c44b12385bb6d651920abd7e1706dca4820b
ChillerDragon <ChillerDragon@gmail.com>
2018-09-16 18:35:08 +0200

    Add web github update script

--- global.sh
+++ global.sh
@@ -2,8 +2,15 @@
 cd /home/chiller/ddpp_database # so the secrets import dont cause trobo
 source secrets.sh
 
+ddpp_user=$USER
+DDPP_USER=chiller
+
 EC_CHILLBLOCK_PORT=1333
 EC_CHILLBLOCK_PASS=$SECRET_EC_CHILLBLOCK_PASS # secrets.sh
 
 EC_BLMAPCHILL_PORT=1336
 EC_BLMAPCHILL_PASS=$SECRET_EC_BLMAPCHILL_PASS # secrets.sh
+
+CB_PATH=/home/$ddpp_user/chillblock
+BL_PATH=/home/$ddpp_user/BlmapChill
+SRC_PATH=/home/$ddpp_user/git/DDNetPP
---
+++ web_scripts/github_update.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+source ../global.sh
+echo "----------------------------------<br/>"
+echo " ---> GITHUB <---<br/>"
+echo "ChillerDragon's mod update script (debug)<br/>"
+echo "chillblock_srv_d (chillblock5)<br/>"
+echo "BlmapChill_srv_d (BlmapChill)<br/>"
+echo "----------------------------------<br/>"
+echo "This script fetches the git/DDNetPP/server and replaces the chillblock/server"
+echo "Server keeps running<br/>"
+
+if [ "$USER" != "$DDPP_USER" ]
+then
+    echo "Error: you=$USER != ddpp=$DDPP_USER"
+fi
+
+# BUILD
+echo "[SRC] fetch and build new source<br/>"
+cd $SRC_PATH
+git pull
+./bam server_debug
+echo "<br/>"
+
+# BACKUP
+echo "[BACKUP] BlmapChill server<br/>"
+cd $BL_PATH;
+mv BlmapChill_srv_d BlmapChill_srv_d_old
+
+echo "[BACKUP] chillblock server<br/>"
+cd $CB_PATH;
+mv chillblock_srv_d chillblock_srv_d_old
+
+# UPDATE
+
+echo "[UPDATE] BlmapChill server<br/>"
+cd $BL_PATH;
+cp $SRC_PATH/DDNetPP_d BlmapChill_srv_D
+
+echo "[UPDATE] chillblock server<br/>"
+cd $CB_PATH;
+cp $SRC_PATH/DDNetPP_d chillblock_srv_d
+
+echo "[UPDATE] maps BlmapChill<br/>"
+cp $SRC_PATH/maps/* $BL_PATH/maps/
+echo "[UPDATE] maps chillblock<br/>"
+cp $SRC_PATH/maps/* $CB_PATH/maps/
