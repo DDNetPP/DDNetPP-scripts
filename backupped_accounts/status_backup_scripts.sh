9b1468c889e5a2184b4a0644f1f1688af61f159f
ChillerDragon <ChillerDragon@gmail.com>
2019-03-28 13:26:55 +0100

    Only show running scripts not grep command

--- backupped_accounts/status_backup_scripts.sh
+++ backupped_accounts/status_backup_scripts.sh
@@ -4,6 +4,6 @@ echo "kill (process id)";
 echo "to kill the script";
 echo "the process id is the first num after the user:";
 echo "==========================";
-ps -ef | grep "./backup_accounts.sh";
-ps -ef | grep "./backup_longterm.sh";
-ps -ef | grep "./backup_records.sh"
+ps -ef | grep -v "grep" | grep "./backup_accounts.sh";
+ps -ef | grep -v "grep" | grep "./backup_longterm.sh";
+ps -ef | grep -v "grep" | grep "./backup_records.sh"
