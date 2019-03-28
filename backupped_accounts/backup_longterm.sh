66a95e8fad926eb81be69348c164eccdebd3ac2d
ChillerDragon <ChillerDragon@gmail.com>
2019-03-28 13:23:01 +0100

    Also slow down accounts database backup

--- backupped_accounts/backup_longterm.sh
+++ backupped_accounts/backup_longterm.sh
@@ -5,6 +5,6 @@ do
     cp ../accounts.db longterm_accounts/accounts_tmp_$(date +%F_%H-%M-%S).db
     #delete files older than 100 days
     find longterm_accounts/ -name "accounts_tmp_*.db" -mtime +100 -type f -delete
-    sleep 6h
+    sleep 48h
 done
 
