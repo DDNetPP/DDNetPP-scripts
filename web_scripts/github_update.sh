d4c6a0388b25144ea0f4b1ae0d228ad2b446aeea
ChillerDragon <ChillerDragon@gmail.com>
2019-08-19 14:22:06 +0000

    Update web server update to use servers script

--- web_scripts/github_update.sh
+++ web_scripts/github_update.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 source ../global.sh
-echo "----------------------------------<br/>"
-echo " ---> GITHUB <---<br/>"
-echo "ChillerDragon's mod update script (debug)<br/>"
-echo "chillblock_srv_d (chillblock5)<br/>"
-echo "BlmapChill_srv_d (BlmapChill)<br/>"
-echo "----------------------------------<br/>"
-echo "This script fetches the git/DDNetPP/server and replaces the chillblock/server"
-echo "Server keeps running<br/>"
+echo "----------------------------------"
+echo " ---> GITHUB <---"
+echo "ChillerDragon's mod update script (debug)"
+echo "chillblock_srv_d (chillblock5)"
+echo "BlmapChill_srv_d (BlmapChill)"
+echo "----------------------------------"
+echo "Server keeps running"
 
 if [ "$USER" != "$DDPP_USER" ]
 then
@@ -15,34 +14,15 @@ then
     exit
 fi
 
-# BUILD
-echo "[SRC] fetch and build new source<br/>"
-cd $SRC_PATH
-git pull
-git submodule update
-./bam server_debug
-echo "<br/>"
+# yes | ./github_update.sh
+# does not work because some newlines get interpreted as default N
+# so we have to spam only y without newlines
+# while true;do printf "y";done | ./github_update.sh
+# didn't work either somehow the whole website hung up lol
 
-# BACKUP
-echo "[BACKUP] BlmapChill server<br/>"
-cd $BL_PATH;
-mv BlmapChill_srv_d BlmapChill_srv_d_old
+cd $CB_PATH
+./github_update.sh -y
 
-echo "[BACKUP] chillblock server<br/>"
-cd $CB_PATH;
-mv chillblock_srv_d chillblock_srv_d_old
+cd $BL_PATH
+./github_update.sh -y
 
-# UPDATE
-
-echo "[UPDATE] BlmapChill server<br/>"
-cd $BL_PATH;
-cp $SRC_PATH/DDNetPP_d BlmapChill_srv_d
-
-echo "[UPDATE] chillblock server<br/>"
-cd $CB_PATH;
-cp $SRC_PATH/DDNetPP_d chillblock_srv_d
-
-echo "[UPDATE] maps BlmapChill<br/>"
-cp $SRC_PATH/maps/* $BL_PATH/maps/
-echo "[UPDATE] maps chillblock<br/>"
-cp $SRC_PATH/maps/* $CB_PATH/maps/
